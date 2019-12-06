b2a83ff8539600c4abbb00d1600d11ee1638d231
Mahfouz-z <43938352+Mahfouz-z@users.noreply.github.com>
2019-12-06 14:05:40 +0200

    Update run_from_terminal.py

--- lef-parser-master/run_from_terminal.py
+++ lef-parser-master/run_from_terminal.py
@@ -6,4 +6,4 @@ elif(sys.argv[1].find(".lef") == -1 or sys.argv[2].find(".def") == -1):
     sys.exit("Arguments should be passed: python <script_name>.py <path/lef_name>.lef <path/def_name>.def")
 else:
     lef_file_name = sys.argv[1]
-    def_file_name = sys.argv[0]
+    def_file_name = sys.argv[2]
