75de6792788f969daca56f922ccb9eafa6ff2ec0
Mahfouz-z <43938352+Mahfouz-z@users.noreply.github.com>
2019-12-06 16:13:46 +0200

    Update name_remap.py

--- lef-parser-master/name_remap.py
+++ lef-parser-master/name_remap.py
@@ -1,6 +1,9 @@
 from def_parser import *
 from lef_parser import *
 
+
+
+
 lef_parser = LefParser("def_lef_files/osu035.lef")
 lef_parser.parse()
 
@@ -11,15 +14,17 @@ def_parser.parse()
 
 def remap_names():
     name_counter = 0
-    map_of_cells_names = []
-    for key in def_parser.components.comp_dict:
+    map_of_names = []
+    for key in def_parser.nets.net_dict:
         new_name = []
-        new_name.append(def_parser.components.comp_dict[key].name)
-        def_parser.components.comp_dict[key].name = "*" + str(name_counter)
-        new_name.append(def_parser.components.comp_dict[key].name)
+        new_name.append(def_parser.nets.net_dict[key].name)
+        def_parser.nets.net_dict[key].name = "*" + str(name_counter)
+        new_name.append(def_parser.nets.net_dict[key].name)
         name_counter += 1
-        map_of_cells_names.append(new_name)
-    print(map_of_cells_names)
+        map_of_names.append(new_name)
+    return(map_of_names)
     
 
-remap_names()
+map_of_names = remap_names()
+    
+print(map_of_names)
